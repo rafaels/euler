2f0b2d6796fd46ab64c3d429c34fb287df3171d5
Elomar + Rafael <elomar+rafael@maisweb.org>
2009-12-09 18:05:55 -0300

    Usando inject

--- euler.rb
+++ euler.rb
@@ -1,6 +1,5 @@
-soma = 0
-(1..999).select { |i| i % 3 == 0 or i % 5 == 0 }.each do |i|
-  soma += i
+soma = (1..999).select { |i| i % 3 == 0 or i % 5 == 0 }.inject do |parcial, i|
+  parcial + i
 end
 
 puts soma
