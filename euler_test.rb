c128f94bc789b35985dcb300d450186ecfaf2872
Elomar França <elomar@maisweb.org>
2009-12-09 20:27:01 -0300

    Primeiro teste

--- euler_test.rb
+++ euler_test.rb
@@ -2,9 +2,10 @@ require 'minitest/autorun'
 require 'euler'
 
 class EulerTest < MiniTest::Unit::TestCase
+  include Euler
 
-  def test_the_truth
-    assert true
+  def test_soma_ate_10
+    assert_equal 23, soma(10)
   end
 
 end
