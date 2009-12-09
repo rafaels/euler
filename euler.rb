5c327bc5e0cca7351021cc3b98be4186a83ff98e
Elomar + Rafael <elomar+rafael@maisweb.org>
2009-12-09 18:15:39 -0300

    preview do que vem por aí ;)

--- euler.rb
+++ euler.rb
@@ -1,5 +1,3 @@
-soma = (1...1000).inject(0) do |parcial, i|
-  (i % 3 == 0 or i % 5 == 0) ? parcial + i : parcial
-end
+soma = (1...1000).select {|i| i % 3 == 0 or i % 5 == 0}.reduce(&:+)
 
 puts soma
